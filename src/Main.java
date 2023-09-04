8ade9fab80cdc6776043643019213e2c40c6b2e6
Felicitas <felicitas.aguerralde@gmail.com>
2023-09-04 11:16:14 -0300

    Andando, ver si persona es abstract

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
         Persona persona1 = new Persona("Juan", "Pérez", 24); //Persona es abstract?
         Empleado empleado1 = new Empleado("Raúl", "Rodriguez", 24,3325632, 500000);
         Empleado empleado2 = new Empleado("Vera", "Margarita", 32,3261598, 400000);
-
         Jerarquico jerarquico1 = new Jerarquico("María", "Álvarez", 34, 3325632, 500000);
+        UsuarioFinal usuarioFinal1 = new UsuarioFinal("Paulo", "Gómez",50,"Paulin77", "P123blin");
 
         //Agrego empleados a jerarquico 1
         jerarquico1.addEmpleadoAcargo(empleado1);
@@ -15,5 +15,7 @@ public class Main {
         System.out.println("-----------Persona 1:----------\n"+persona1.toString());
         System.out.println("-----------Empleado 1:----------\n"+empleado1.toString());
         System.out.println("-----------Jerárquico 1:----------\n"+jerarquico1.toString());
+        System.out.println("-----------Usuario Final 1:----------\n"+usuarioFinal1.toString());
+
     }
 }
